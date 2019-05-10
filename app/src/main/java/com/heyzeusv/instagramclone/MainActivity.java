4aae72771308924d2c55634f8d4f32cdf598d44e
HeyZeusV <valdesj419@gmail.com>
2019-05-10 01:11:24 -0700

    Got log in/sign up screen up and running

--- app/src/main/java/com/heyzeusv/instagramclone/MainActivity.java
+++ app/src/main/java/com/heyzeusv/instagramclone/MainActivity.java
@@ -2,14 +2,97 @@ package com.heyzeusv.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Boolean signUpModeActive = true;
+    TextView logInTextView;
+
+    public void switcher(View view) {
+
+        Button signUpButton = findViewById(R.id.signUpButton);
+        if (signUpModeActive) {
+
+            signUpModeActive = false;
+            signUpButton.setText("Login");
+            logInTextView.setText("Or, Sign Up");
+        } else {
+
+            signUpModeActive = true;
+            signUpButton.setText("Sign Up");
+            logInTextView.setText("Or, Log In");
+        }
+    }
+
+    public void signUpClicked(View view) {
+
+        EditText usernameEditText = findViewById(R.id.usernameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        if (usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+
+            Toast.makeText(this, "A username and a password are required.", Toast.LENGTH_SHORT).show();
+        } else {
+
+            if (signUpModeActive) {
+
+                ParseUser user = new ParseUser();
+                user.setUsername(usernameEditText.getText().toString());
+                user.setPassword(passwordEditText.getText().toString());
+                user.signUpInBackground(new SignUpCallback() {
+
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+
+                            Log.i("Signup", "Success!");
+                        } else {
+
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else {
+
+                ParseUser.logInInBackground(usernameEditText.getText().toString(), passwordEditText.getText().toString(), new LogInCallback() {
+
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+
+                        if (user != null) {
+
+                            Log.i("Log In", "Okay!");
+                        } else {
+
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logInTextView = findViewById(R.id.logInTextView);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
--- app/src/main/java/com/heyzeusv/instagramclone/StarterApplication.java
+++ app/src/main/java/com/heyzeusv/instagramclone/StarterApplication.java
@@ -5,10 +5,6 @@ import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 public class StarterApplication extends Application {
 
@@ -24,25 +20,7 @@ public class StarterApplication extends Application {
         .server("http://18.224.15.183:80/parse/")
         .build());
 
-        ParseObject object = new ParseObject("ExampleObject");
-        object.put("myNumber", "123");
-        object.put("myString", "HeyZeus");
-
-        object.saveInBackground(new SaveCallback() {
-
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-
-                    Log.i("Parse Result", "Successful!");
-                } else {
-
-                    Log.i("Parse Result", "Failed" + e.toString());
-                }
-            }
-        });
-
-        ParseUser.enableAutomaticUser();
+        // ParseUser.enableAutomaticUser();
 
         ParseACL defaultACL = new ParseACL();
         defaultACL.setPublicWriteAccess(true);
